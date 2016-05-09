930fc6959edfa4d1ff5c9281171d87e77e37d69d
davidkatz <15Dkatz@shcp.edu>
2016-05-09 09:08:59 -0700

    Finished tutorial.

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,143 @@
-// var line = re');
-// import line?
+const Line = React.createClass({
+  propTypes: {
+    path:         React.PropTypes.string.isRequired,
+    stroke:       React.PropTypes.string,
+    fill:         React.PropTypes.string,
+    strokeWidth:  React.PropTypes.number
+  },
 
+  getDefaultProps() {
+    return {
+      stroke:       'blue',
+      fill:         'none',
+      strokeWidth:  3
+    };
+  },
 
-var Hello = React.createClass({
-  render: function() {
-    return <div > Hello {
-      this.props.name
-    } < /div>;
+  render() {
+    let { path, stroke, fill, strokeWidth } = this.props;
+    return (
+      <path
+        d={path}
+        fill={fill}
+        stroke={stroke}
+        strokeWidth={strokeWidth}
+        />
+    );
   }
+
 });
 
-ReactDOM.render( < Hello name = "World" / > ,
+const DataSeries = React.createClass({
+
+  propTypes: {
+    colors:             React.PropTypes.func,
+    data:               React.PropTypes.object,
+    interpolationType:  React.PropTypes.string,
+    xScale:             React.PropTypes.func,
+    yScale:             React.PropTypes.func
+  },
+
+  getDefaultProps() {
+    return {
+      data:               [],
+      interpolationType:  'cardinal',
+      colors:             d3.scale.category10()
+    };
+  },
+
+  render() {
+    let { data, colors, xScale, yScale, interpolationType } = this.props;
+
+    let line = d3.svg.line()
+      .interpolate(interpolationType)
+      .x((d) => { return xScale(d.x); })
+      .y((d) => { return yScale(d.y); });
+
+    let lines = data.points.map((series, id) => {
+      return (
+        <Line
+          path={line(series)}
+          stroke={colors(id)}
+          key={id}
+          />
+      );
+    });
+
+    return (
+      <g>
+        <g>{lines}</g>
+      </g>
+    );
+  }
+
+});
+
+
+const LineChart = React.createClass({
+
+  propTypes: {
+    width:  React.PropTypes.number,
+    height: React.PropTypes.number,
+    data:   React.PropTypes.object.isRequired
+  },
+
+  // sets a default value on the given propTypes if none argued in the ReactDOM.
+  getDefaultProps(){
+    return {
+      width:  600,
+      height: 300
+    }
+  },
+
+  render() {
+    let { width, height, data } = this.props;
+
+    let xScale = d3.scale.ordinal()
+                   .domain(data.xValues)
+                   .rangePoints([0, width]);
+
+    let yScale = d3.scale.linear()
+                   .range([height, 10])
+                   .domain([data.yMin, data.yMax]);
+
+    return (
+      <svg width={width} height={height}>
+          <DataSeries
+            xScale={xScale}
+            yScale={yScale}
+            data={data}
+            width={width}
+            height={height}
+            />
+      </svg>
+    );
+  }
+
+});
+
+
+let xdata = {
+  points: [
+    [ { x: 0, y: 20 }, { x: 1, y: 30 }, { x: 2, y: 10 }, { x: 3, y: 5 },
+      { x: 4, y: 8 }, { x: 5, y: 15 }, { x: 6, y: 10 } ]
+    ,
+    [ { x: 0, y: 8 }, { x: 1, y: 5 }, { x: 2, y: 20 }, { x: 3, y: 12 },
+      { x: 4, y: 4 }, { x: 5, y: 6 }, { x: 6, y: 2 } ]
+    ,
+    [ { x: 0, y: 0 }, { x: 1, y: 5 }, { x: 2, y: 8 }, { x: 3, y: 2 },
+      { x: 4, y: 6 }, { x: 5, y: 4 }, { x: 6, y: 2 } ]
+    ],
+  xValues: [0,1,2,3,4,5,6],
+  yMin: 0,
+  yMax: 30
+};
+
+ReactDOM.render(
+  <LineChart
+    data={xdata}
+    width={600}
+    height={300}
+    />,
   document.getElementById('container')
 );
-
-// require line?
-// insert line somehow into render
-
-
