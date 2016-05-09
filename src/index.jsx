1781b8c6e80b8e25ea467b070a2d2318a73c7768
davidkatz <15Dkatz@shcp.edu>
2016-05-09 00:50:23 -0700

    boilerplate commit

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+// var line = re');
+// import line?
+
+
+var Hello = React.createClass({
+  render: function() {
+    return <div > Hello {
+      this.props.name
+    } < /div>;
+  }
+});
+
+ReactDOM.render( < Hello name = "World" / > ,
+  document.getElementById('container')
+);
+
+// require line?
+// insert line somehow into render
+
+
